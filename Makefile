d6c0083dd2fb542c2c38bd9ca52756607b8526ee
jon <jon@01fca44a-87da-11e1-a91c-b16e236fe354>
2012-04-16 17:08:05 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/jmc/nes@2 01fca44a-87da-11e1-a91c-b16e236fe354

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: rom1.nes
+
+rom1.nes: rom1.asm
+	nesasm3 -s rom1.asm
+
+clean:
+	rm -v *.nes *.o65
---
+++ rom1.asm
@@ -0,0 +1,19 @@
+
+.inesprg	1
+.ineschr	1
+.inesmap	0
+.inesmir	1
+
+.bank		0
+.org		$0x8000
+Start
+
+.bank		1
+.org		$0xFFFA
+.dw		Start
+.dw		Start
+.dw		Start
+
+.bank		2
+.org		$0x0000
+
